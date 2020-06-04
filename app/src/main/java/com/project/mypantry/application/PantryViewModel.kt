c8610d5a53db51de24a97dc61541ebbcb463402a
towm1204 <17towm@gmail.com>
2020-06-03 22:40:46 -0700

    tow's merge

--- app/src/main/java/com/project/mypantry/application/PantryViewModel.kt
+++ app/src/main/java/com/project/mypantry/application/PantryViewModel.kt
@@ -6,16 +6,14 @@ import com.project.mypantry.objects.IngredientInstance
 
 class PantryViewModel: ViewModel() {
 
-    lateinit var expired = MutableLiveData<Boolean>(false)
-
-    lateinit var pantryListManager: PantryListManager
-    lateinit var apiManager: HTTPManager
-
-    fun init(pantryListManager: PantryListManager, apiManager: HTTPManager) {
-        this.pantryListManager = pantryListManager
-        pantryList = MutableLiveData<List<IngredientInstance>>(this.pantryListManager.pantry)
-        this.apiManager = apiManager
-    }
-
-    fun delete
+//    lateinit var expired = MutableLiveData<Boolean>(false)
+//
+//    lateinit var pantryListManager: PantryListManager
+//    lateinit var apiManager: HTTPManager
+//
+//    fun init(pantryListManager: PantryListManager, apiManager: HTTPManager) {
+//        this.pantryListManager = pantryListManager
+//        pantryList = MutableLiveData<List<IngredientInstance>>(this.pantryListManager.pantry)
+//        this.apiManager = apiManager
+//    }
 }
