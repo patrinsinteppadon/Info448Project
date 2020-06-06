3484d6788c5310dff735d1ec81ba755409e5512b
towm1204 <17towm@gmail.com>
2020-06-06 00:37:06 -0700

    implemented search bar, needs to update adapter

--- app/src/main/java/com/project/mypantry/GlossarySearchActivity.kt
+++ app/src/main/java/com/project/mypantry/GlossarySearchActivity.kt
@@ -1,12 +1,17 @@
 package com.project.mypantry
 
+import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.widget.Adapter
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import com.project.mypantry.adapters.GlossaryListAdapter
 import com.project.mypantry.application.PantryApp
 import com.project.mypantry.objects.IngredientType
@@ -14,6 +19,9 @@ import kotlinx.android.synthetic.main.activity_glossary_search.*
 
 class GlossarySearchActivity : AppCompatActivity() {
     lateinit var listView: ListView
+    lateinit var app: PantryApp
+    lateinit var adapter: GlossaryListAdapter
+
 
     companion object {
         const val FOR_PANTRY = "ripblackmambathelegend24"
@@ -24,11 +32,11 @@ class GlossarySearchActivity : AppCompatActivity() {
         setContentView(R.layout.activity_glossary_search)
         title = "Pick an Ingredient"
 
-        val app: PantryApp = (applicationContext as PantryApp)
+        app = (applicationContext as PantryApp)
         val glossaryManager = app.glossaryManager
+        adapter = GlossaryListAdapter(this, glossaryManager.glossary)
 
         listView = lvIngredientType
-        val adapter = GlossaryListAdapter(this, glossaryManager.glossary)
         listView.adapter = adapter
 
         listView.setOnItemClickListener { _, _, _, id ->
@@ -46,7 +54,37 @@ class GlossarySearchActivity : AppCompatActivity() {
             }
         }
 
-        // set on text change listener
+        // set up search bar
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.glossary_search_menu, menu)
+
+        val manager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchItem = menu?.findItem(R.id.search_bar)
+        val searchView = searchItem?.actionView as SearchView
+
+        searchView.setSearchableInfo(manager.getSearchableInfo(componentName))
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchView.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    Log.i("Toww", app.glossaryManager.search(newText).toString())
+                }
+                return true
+            }
+
+        })
+
+        return true
 
     }
 }
---
+++ app/src/main/java/com/project/mypantry/viewModels/GlossarySearchViewModel.kt
@@ -0,0 +1,8 @@
+package com.project.mypantry.viewModels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GlossarySearchViewModel: ViewModel() {
+    //var searchList = MutableLiveData<>
+}
