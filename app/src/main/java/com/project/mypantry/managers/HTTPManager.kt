cf7c4f7fb3d227471ded479b06f883ae59e56cf5
LiamAlbright <albrightliam56@gmail.com>
2020-06-09 13:45:55 -0700

    trying http stuff

--- app/src/main/java/com/project/mypantry/managers/HTTPManager.kt
+++ app/src/main/java/com/project/mypantry/managers/HTTPManager.kt
@@ -1,6 +1,8 @@
 package com.project.mypantry.managers
 
 import com.project.mypantry.managers.JsonResponse
+import retrofit2.Call
+import retrofit2.http.GET
 
 /**
  * responseAsObject will be null until the results are loaded in
@@ -9,4 +11,8 @@ interface HTTPManager {
     var responseAsObject: JsonResponse?
 
     fun downloadLists()
+
+    @GET("LiamAlbright/codesnippetspantry/master/Recipes.json")
+    fun allPantryRep (): Call<pantryResultsMod>
+
 }
--- app/src/main/java/com/project/mypantry/objects/Recipe.kt
+++ app/src/main/java/com/project/mypantry/objects/Recipe.kt
@@ -1,12 +1,26 @@
 package com.project.mypantry.objects
 
 import android.os.Parcelable
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
+
 
 @Parcelize
 data class Recipe (
+    @SerializedName("id")
+    @Expose
     var id: Int,
+
+    @SerializedName("name")
+    @Expose
     var name: String,
-    var img: String, // unsure what data type should be
+
+    @SerializedName("img")
+    @Expose
+    var img: String,
+
+    @SerializedName("ingredients")
+    @Expose
     val ingredients: List<IngredientType>
 ): Parcelable
