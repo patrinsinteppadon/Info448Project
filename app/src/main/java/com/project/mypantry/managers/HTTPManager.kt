3d197895ae287240adb8365a65108e19dea757a3
LiamAlbright <albrightliam56@gmail.com>
2020-06-09 14:16:05 -0700

    fixmerge

--- app/src/main/java/com/project/mypantry/managers/HTTPManager.kt
+++ app/src/main/java/com/project/mypantry/managers/HTTPManager.kt
@@ -1,8 +1,6 @@
 package com.project.mypantry.managers
 
 import com.project.mypantry.managers.JsonResponse
-import retrofit2.Call
-import retrofit2.http.GET
 
 /**
  * responseAsObject will be null until the results are loaded in
@@ -11,8 +9,4 @@ interface HTTPManager {
     var responseAsObject: JsonResponse?
 
     fun downloadLists()
-
-    @GET("LiamAlbright/codesnippetspantry/master/Recipes.json")
-    fun allPantryRep (): Call<pantryResultsMod>
-
 }
